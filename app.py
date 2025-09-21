3f48b3dd85f03dc14c3b5c9b1ca73ac9902e58bc
Ganesh Kamat <kamatgc@gmail.com>
2025-09-21 17:45:56 +0530

    align with localhost 3

--- app.py
+++ app.py
@@ -287,4 +287,4 @@ def ask_ai():
 
 if __name__ == '__main__':
     print("SERVER: Starting Flask application...")
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
